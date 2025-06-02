501afa820b4abed23e5e6985ec77b34e0476fce0
Maniakalochka <prochorwolochoff@gmail.com>
2025-06-02 15:26:21 +0300

    feat: implement RetrieveRemoteData and Fibo classes with iteration support

--- src/iterators/utils.py
+++ src/iterators/utils.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from itertools import batched
-from typing import Iterable, TypeAlias
+from typing import Any, Iterable, Self, TypeAlias
 
 SomeRemoteData: TypeAlias = int
 
@@ -29,8 +29,43 @@ def request(query: Query) -> Page:
 
 
 class RetrieveRemoteData:
-    pass
+    def __init__(self, per_page: int = 3, page: int = 1) -> None:
+        self.query = Query()
+        self.page = request(self.query)
+        self.count = 0
+
+    def __iter__(self) -> Self:
+        return self
+
+    def __next__(self) -> Any:
+        if self.count >= len(self.page.results):
+            if self.page.next is None:
+                raise StopIteration
+
+            self.query.page = self.page.next
+            self.page = request(self.query)
+            self.count = 0
+
+        val = self.page.results[self.count]
+        self.count += 1
+        return val
 
 
 class Fibo:
-    pass
+    def __init__(self, n: int) -> None:
+        self.n = n
+        self.count = 0
+        self.a = 0
+        self.b = 1
+
+    def __iter__(self) -> Self:
+        return self
+
+    def __next__(self):
+        if self.count >= self.n:
+            raise StopIteration
+
+        val = self.a
+        self.a, self.b = self.b, self.a + self.b
+        self.count += 1
+        return val
